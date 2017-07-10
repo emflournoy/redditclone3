003122e066cced98ffbe6a535961fa26f8e48629
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-10 17:17:18 -0600

    clean up logs

--- app/public/posts/comments.component.js
+++ app/public/posts/comments.component.js
@@ -16,30 +16,13 @@
         vm.submitCom = function(comment) {
           console.log(comment);
           let commentObj = {content: comment};
-          // commentObj.content = comment;
           postService.makeComment(vm.post, commentObj)
           .then(function (response){
-            // console.log(response);
             vm.post.comments.push(response);
           })
           console.log(vm.newcomment);
           delete vm.newcomment;
         }
-
-        // vm.submitCom = function(comment) {
-        //   let commentObj = {};
-        //   console.log(vm.post.id);
-        //   commentObj.content = comment;
-        //   commentObj.created_at = new Date();
-        //   // commentObj.post_id = post.id;
-        //   $http.post(`api/posts/${vm.post.id}/comments`,
-        //    comment).then(function (response){
-        //      console.log(response);
-        //     post.comments.push(comment);
-        //   })
-        //   // delete post.comment;
-        // }
-
 
       }
 
